75020b4a83c215cc251d2e54b10ac0fc1aa3e473
matthewdovey <matthewdovey@hotmail.co.uk>
2015-10-25 22:28:08 +0000

    set up second activity

--- app/src/main/java/com/matthew/whatsonandroidapp/EventActivity.java
+++ app/src/main/java/com/matthew/whatsonandroidapp/EventActivity.java
@@ -8,7 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class EventActivity extends AppCompatActivity {
 
@@ -17,7 +20,16 @@ public class EventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
 
+        ImageView eventImage = (ImageView) findViewById(R.id.focusImageId);
+        TextView eventTitle = (TextView) findViewById(R.id.focusTitleId);
+        TextView eventInfo = (TextView) findViewById(R.id.focusInfoId);
+        TextView eventDesc = (TextView) findViewById(R.id.focusDescId);
         Button webEvent = (Button) findViewById(R.id.webButton);
+
+        Picasso.with(eventImage.getContext()).load(MainActivity.focusImage).into(eventImage);
+        eventTitle.setText(MainActivity.focusTitle);
+        eventInfo.setText(MainActivity.focusInfo);
+        eventDesc.setText(MainActivity.focusDesc);
 
         webEvent.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/matthew/whatsonandroidapp/MainActivity.java
+++ app/src/main/java/com/matthew/whatsonandroidapp/MainActivity.java
@@ -38,16 +38,29 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    static String focusImage;
+    static String focusTitle;
+    static String focusInfo;
+    static String focusDesc;
+
+    //private ArrayList<String> eventLinks = new ArrayList<String>();
+    private ArrayList<String> eventTitles = new ArrayList<String>();
+    private ArrayList<String> eventInfos = new ArrayList<String>();
+    private ArrayList<String> eventImages = new ArrayList<String>();
+    private ArrayList<String> eventDescs = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -56,25 +69,84 @@ public class MainActivity extends AppCompatActivity {
 
         new JsoupListView().execute();
 
-        Button eventPage = (Button) findViewById(R.id.button);
+        /*Button eventPage = (Button) findViewById(R.id.button1);
         eventPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, EventActivity.class));
             }
-        });
+        });*/
+
+        Button event1 = (Button) findViewById(R.id.button1);
+        Button event2 = (Button) findViewById(R.id.button2);
+        Button event3 = (Button) findViewById(R.id.button3);
+        Button event4 = (Button) findViewById(R.id.button4);
+        Button event5 = (Button) findViewById(R.id.button5);
+        Button event6 = (Button) findViewById(R.id.button6);
+        Button event7 = (Button) findViewById(R.id.button7);
+        Button event8 = (Button) findViewById(R.id.button8);
+        Button event9 = (Button) findViewById(R.id.button9);
+        Button event10 = (Button) findViewById(R.id.button10);
+        Button event11 = (Button) findViewById(R.id.button11);
+        Button event12 = (Button) findViewById(R.id.button12);
+        Button event13 = (Button) findViewById(R.id.button13);
+        Button event14 = (Button) findViewById(R.id.button14);
+        Button event15 = (Button) findViewById(R.id.button15);
+
+        event1.setOnClickListener(buttonCheck);
+        event2.setOnClickListener(buttonCheck);
+        event3.setOnClickListener(buttonCheck);
+        event4.setOnClickListener(buttonCheck);
+        event5.setOnClickListener(buttonCheck);
+        event6.setOnClickListener(buttonCheck);
+        event7.setOnClickListener(buttonCheck);
+        event8.setOnClickListener(buttonCheck);
+        event9.setOnClickListener(buttonCheck);
+        event10.setOnClickListener(buttonCheck);
+        event11.setOnClickListener(buttonCheck);
+        event12.setOnClickListener(buttonCheck);
+        event13.setOnClickListener(buttonCheck);
+        event14.setOnClickListener(buttonCheck);
+        event15.setOnClickListener(buttonCheck);
+
+
+
+
+        event14.setOnClickListener(buttonCheck);
     }
+
+    View.OnClickListener buttonCheck = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            int longId = v.getId();
+            boolean buttFinder = false;
+            int idNumber = 1;
+
+            while (!buttFinder) {
+                String id = "button"+idNumber;
+                int resID = getResources().getIdentifier(id, "id", "com.matthew.whatsonandroidapp");
+                if (longId == resID) {
+                    buttFinder = true;
+                    idNumber--;
+                }
+                idNumber++;
+                System.out.println(idNumber);
+            }
+            System.out.println(idNumber);
+
+            focusImage = eventImages.get(idNumber);
+            focusTitle = eventTitles.get(idNumber);
+            focusInfo = eventInfos.get(idNumber);
+            focusDesc = eventDescs.get(idNumber);
+
+            startActivity(new Intent(MainActivity.this, EventActivity.class));
+        }
+    };
 
     private class JsoupListView extends AsyncTask<ArrayList<String>,Void,ArrayList<String>>{
 
         private int x;
         private int itemNumber = x;
-
-        private ArrayList<String> eventLinks = new ArrayList<String>();
-        private ArrayList<String> eventTitles = new ArrayList<String>();
-        private ArrayList<String> eventInfos = new ArrayList<String>();
-        private ArrayList<String> eventImages = new ArrayList<String>();
-        private ArrayList<String> eventDescs = new ArrayList<String>();
 
         @Override
         protected ArrayList<String> doInBackground(ArrayList<String>... params) {
@@ -97,11 +169,13 @@ public class MainActivity extends AppCompatActivity {
                 }
                 whatson.getImages(itemNumber);
 
-                eventLinks = whatson.returnLinks();
+                //eventLinks = whatson.returnLinks();
                 eventImages = whatson.returnImages();
                 eventTitles = whatson.returnTitles();
                 eventInfos = whatson.returnInfos();
                 eventDescs = whatson.returnDescs();
+
+                System.out.println("Stage one complete");
             }
             return null;
         }
@@ -111,27 +185,73 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(ArrayList<String> strings) {
             super.onPostExecute(strings);
             String link;
-            int j = 1;
+            String buttName;
+            String infoName;
+            String descName;
 
-            String imgId = "imageView"+j;
+            System.out.println("stage two");
+
+            //J might be changed by each for loop fucking up the other for loops.
+            //Might need to put j=1 in each for loop or see if you can protect it.
+            int j=1;
+
             for(int i=0;i<15;i++) {
+                String imgId = "imageView"+j;
                 if (j == 1) {
                     ImageView changeImage = (ImageView) findViewById(R.id.imageView);
                     link = eventImages.get(i);
                     Picasso.with(changeImage.getContext()).load(link).into(changeImage);
                 } else {
+                    System.out.println(j);
+                    System.out.println(imgId);
                     int resID = getResources().getIdentifier(imgId, "id", "com.matthew.whatsonandroidapp");
                     ImageView changeImage = (ImageView) findViewById(resID);
 
                     System.out.println(changeImage);
 
                     link = eventImages.get(i);
-                    /*try {
+                    try {
                         Picasso.with(changeImage.getContext()).load(link).into(changeImage);
                     } catch (Exception e) {
-
-                    }*/
+                        System.out.println("ERROR: Unable to use picture");
+                    }
                 }
+                j++;
+            }
+
+            j=1;
+
+            for(int i=0;i<15;i++) {
+                String butId = "button"+j;
+                int resID = getResources().getIdentifier(butId, "id", "com.matthew.whatsonandroidapp");
+                Button changeButton = (Button) findViewById(resID);
+                buttName = eventTitles.get(i);
+
+                changeButton.setText(buttName);
+                j++;
+            }
+
+            j=1;
+
+            for(int i=0;i<15;i++) {
+                String infoId = "info"+j;
+                int resID = getResources().getIdentifier(infoId, "id", "com.matthew.whatsonandroidapp");
+                TextView changeInfo = (TextView) findViewById(resID);
+                infoName = eventInfos.get(i);
+
+                changeInfo.setText(infoName);
+                j++;
+            }
+
+            j=1;
+
+            for(int i=0;i<15;i++) {
+                String descId = "desc"+j;
+                int resID = getResources().getIdentifier(descId, "id", "com.matthew.whatsonandroidapp");
+                TextView changeDesc = (TextView) findViewById(resID);
+                descName = eventDescs.get(i);
+
+                changeDesc.setText(descName);
                 j++;
             }
         }
